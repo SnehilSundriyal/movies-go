a68aa4f5ad1d49de6ccec14b9d077301f3d23d32
SnehilSundriyal <snehilsundriyal@gmail.com>
2025-06-18 17:09:59 +0530

    json work simplified

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 )
@@ -17,39 +16,22 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 		Version: "1.0.0",
 	}
 
-	out, err := json.Marshal(payLoad)
+	err := app.writeJSON(w, http.StatusOK, payLoad)
 	if err != nil {
 		log.Println(err)
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	_, err = w.Write(out)
-	if err != nil {
-		log.Println(err)
-	}
-
-	w.Header()
 }
 
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
 	movies, err := app.DB.AllMovies()
 	if err != nil {
-		log.Println(err)
+		app.errorJSON(w, err)
+		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-
-	out, err := json.Marshal(movies)
+	err = app.writeJSON(w, http.StatusOK, movies)
 	if err != nil {
 		log.Println(err)
 	}
 
-	w.WriteHeader(http.StatusOK)
-	_, err = w.Write(out)
-	if err != nil {
-		log.Println(err)
-	}
-
-	w.Header()
 }
